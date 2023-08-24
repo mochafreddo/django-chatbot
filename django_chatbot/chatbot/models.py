ca94dd87dcd28e37384d97bdc093c57a28f36cee
Geoffrey Kim <76798197+mochafreddo@users.noreply.github.com>
2023-08-24 17:29:04 +0900

    chore: make general adjustments for compatibility with forked code

--- django_chatbot/chatbot/models.py
+++ django_chatbot/chatbot/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+
 
 # Create your models here.
 class Chat(models.Model):
@@ -9,4 +10,4 @@ class Chat(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.user.username}: {self.message}'
+        return f"{self.user.username}: {self.message}"
